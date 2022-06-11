a68f4b76c1e6a98cf0bea437b83c509481c82a2d
Ellinaa <99786204+Ellinaa@users.noreply.github.com>
2022-06-11 22:01:24 +0800

    Create 3.js

---
+++ Exercise/EX5/3.js
@@ -0,0 +1,12 @@
+function isPrime(number) {
+        var isPrime = true;
+        for (var i = 2; i < number; i++) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+console.log(isPrime(21));
